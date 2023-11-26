9c424133ff708b099aaf02b1bde6245733bfce6e
Alejandro RL <arodriguezl9302@gmail.com>
2023-11-26 04:38:19 +0100

    updates

--- app/components/shared/Comments.tsx
+++ app/components/shared/Comments.tsx
@@ -2,7 +2,6 @@
 
 import { useTheme } from "next-themes";
 import Giscus from "@giscus/react";
-import { giscusRepoId, giscusCategoryId } from "@/lib/env.api";
 
 export default function Comments() {
   const theme = useTheme();
@@ -14,20 +13,21 @@ export default function Comments() {
       : "dark";
 
   return (
-    <Giscus
-      id="comments"
-      repo="evavic44/victoreke.com"
-      repoId={giscusRepoId}
-      category="Announcements"
-      categoryId={giscusCategoryId}
-      mapping="title"
-      term="Welcome to @giscus/react component!"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="bottom"
-      theme={giscusTheme}
-      lang="en"
-      loading="lazy"
-    />
+    // <Giscus
+    //   id="comments"
+    //   repo="evavic44/victoreke.com"
+    //   repoId={giscusRepoId}
+    //   category="Announcements"
+    //   categoryId={giscusCategoryId}
+    //   mapping="title"
+    //   term="Welcome to @giscus/react component!"
+    //   reactionsEnabled="1"
+    //   emitMetadata="0"
+    //   inputPosition="bottom"
+    //   theme={giscusTheme}
+    //   lang="en"
+    //   loading="lazy"
+    // />
+    <></>
   );
 }
